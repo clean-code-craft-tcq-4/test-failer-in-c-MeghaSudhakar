b5f52d037178319940141f824e035d9c2fe5afba
MeghaSudhakar <105205192+MeghaSudhakar@users.noreply.github.com>
2022-06-21 20:38:27 +0530

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -1,26 +1,64 @@
 #include <stdio.h>
 #include <assert.h>
 
-int printColorMap() 
+const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
+const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
+    
+char AlignmentCheck[1000];
+char ReferenceAlignmentCheck[1000];
+
+int printColorMap(char *AlignmentCheck) 
 {
-      const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
-      const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
- 
-    int i = 0, j = 0;
-    for(i = 0; i < 5; i++) 
+    int minor = 0;
+    int major = 0;
+    int pairnum = 0;
+    
+    for(major=0; major<5; major++)
     {
-        for(j = 0; j < 5; j++) 
+        for(minor=0; minor<5; minor++)
         {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[j]);            
-        } 
+            pairnum += sprintf(&AlignmentCheck[pairnum], "%d |%s |%s\n",((major * 5 + minor) + 1), (majorColor[major]), (minorColor[minor]));
+        }
     }
-    return i * j;
+    printf("Below is the Actual manual color code display which has to be checked for Alignment\n%s\n",AlignmentCheck);
+    
+    return minor*major;
 }
 
+void ReferenceManual_ColorCode(char *ReferenceAlignmentCheck)
+{
+    int minor = 0;
+    int major = 0;
+    int pairnum = 0;
+    
+    for(major=0; major<5; major++)
+    {
+        for(minor=0; minor<5; minor++)
+        {
+            pairnum += sprintf(&ReferenceAlignmentCheck[pairnum], "%d\t|%s\t\t|%s\n",((major * 5 + minor) + 1), (majorColor[major]), (minorColor[minor]));
+        }
+    }
+    
+    printf("Below is the reference manual color code display\n%s\n",ReferenceAlignmentCheck);
+}
+
+void AlignmentCheck_EvaluatingString()
+{
+    //assert(strcmp(&ReferenceAlignmentCheck[0],&AlignmentCheck[0]) == 0);   
+    int ret = (strcmp(&ReferenceAlignmentCheck[0],&AlignmentCheck[0]) == 0);  
+    if(ret == 0)
+    printf("yse\n");
+    else
+    printf("no\n");
+    
+}
+-
 int main() 
 {
-    int result = printColorMap();
+    int result = printColorMap(AlignmentCheck);
+    ReferenceManual_ColorCode(ReferenceAlignmentCheck);
     assert(result == 25);
+    AlignmentCheck_EvaluatingString();
     printf("All is well (maybe!)\n");
     return 0;
 }
